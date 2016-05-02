e5d562294a6c305971e59c51889a436262d94897
mbusson <mbusson@users.noreply.github.com>
2016-05-02 17:41:34 +0200

    Added files via upload

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Two-Sided Toggle Menu</title>
 	 <link rel='shortcut icon' href='favicon.ico' type='image/x-icon'/ >
 	 <link rel="stylesheet" href="styles.css">
 	 <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
@@ -14,7 +14,41 @@
 	    <div class="loader-section section-right"></div>
 	</div>
 
-	<div id="">
+	<div id="select"> <br> <br>
+		<a href="#" id="rent" class="btn btn-2">
+			Find a place
+		</a>
+		<a href="#" id="rent-out" class="btn btn-2">
+			Rent out
+		</a>
+	</div>
+
+	<div id="rent-menu">
+		<div class="spacer-big"></div>
+		<div class="rent-menu">1</div>
+		<div class="spacer"></div>
+		<div class="rent-menu">2</div>
+		<div class="spacer"></div>
+		<div class="rent-menu">3</div>
+		<div class="spacer"></div>
+		<div class="rent-menu">4</div>
+		<div class="spacer"></div>
+		<div class="rent-menu-exit">Exit this menu</div>
+		<div class="spacer-big"></div>
+	</div>
+
+	<div id="rent-out-menu">
+		<div class="spacer-big"></div>
+		<div class="rent-out-menu">1</div>
+		<div class="spacer"></div>
+		<div class="rent-out-menu">2</div>
+		<div class="spacer"></div>
+		<div class="rent-out-menu">3</div>
+		<div class="spacer"></div>
+		<div class="rent-out-menu">4</div>
+		<div class="spacer"></div>
+		<div class="rent-out-menu-exit">Exit this menu</div>
+		<div class="spacer-big"></div>
 	</div>
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
