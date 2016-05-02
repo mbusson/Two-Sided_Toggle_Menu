a5ed18d65b44bdb6012396a9b5c7097fec1d6340
mbusson <mbusson@users.noreply.github.com>
2016-05-02 17:41:50 +0200

    Added files via upload

--- style.js
+++ style.js
@@ -1,5 +1,25 @@
 $(document).ready(function(){
 
-// --- 
+// --- PRE-LOADER ---
+ 
+    setTimeout(function(){
+        $('body').addClass('loaded');
+    }, 1);
 
+// --- MENU TOGGLE SELECTOR ---
+
+/* count 2 = none, 1 = rent, 0 = rent-out */
+
+	$('#rent').click(function() {
+		$('#rent-menu').css('width', '20%');
+		$('#rent-out-menu').css('width', '1%');
+	});
+	$('#rent-out').click(function() {
+		$('#rent-out-menu').css('width', '20%');
+		$('#rent-menu').css('width', '1%');
+	});
+	$('#exit').click(function() {
+		$('#rent-out-menu').css('width', '1%');
+		$('#rent-menu').css('width', '1%');
+	});
 });
