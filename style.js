237a6451218b88a914d96105ae2f1cfff9be099a
mbusson <mbusson@users.noreply.github.com>
2016-05-03 13:51:56 +0200

    Added files via upload

--- style.js
+++ style.js
@@ -1,29 +1,29 @@
 $(document).ready(function(){
 
-// --- PRE-LOADER ---
- 
-    setTimeout(function(){
-        $('body').addClass('loaded');
-    }, 1);
-
 // --- MENU TOGGLE SELECTOR ---
-
-/* count 2 = none, 1 = rent, 0 = rent-out */
 
 	$('#rent').click(function() {
 		$('#rent-menu').css('width', '20%');
+		$('#rent-menu .rent-txt').fadeIn()
 		$('#rent-out-menu').css('width', '1%');
+		$('#rent-out-menu .rent-out-txt').fadeOut(200);
 	});
 	$('#rent-out').click(function() {
 		$('#rent-out-menu').css('width', '20%');
+		$('#rent-out-menu .rent-out-txt').fadeIn();
 		$('#rent-menu').css('width', '1%');
+		$('#rent-menu .rent-txt').fadeOut(200);
 	});
 	$('#rent-menu .exit').click(function() {
 		$('#rent-out-menu').css('width', '1%');
+		$('#rent-out-menu .rent-out-txt').fadeOut(200);
 		$('#rent-menu').css('width', '1%');
+		$('#rent-menu .rent-txt').fadeOut(200);
 	});
 	$('#rent-out-menu .exit').click(function() {
 		$('#rent-out-menu').css('width', '1%');
+		$('#rent-out-menu .rent-out-txt').fadeOut(200);
 		$('#rent-menu').css('width', '1%');
+		$('#rent-menu .rent-txt').fadeOut(200);
 	});
 });
